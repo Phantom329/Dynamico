76b48b56832ebc9cb533e05cf4b9fcc164fb243b
Linkman3211 <41981872+Linkman3211@users.noreply.github.com>
2018-08-27 21:34:33 -0400

    Add files via upload

---
+++ HammerGander/HammerGander/Game1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace HammerGander
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    
+    // Main
+    public class Game1 : Game
+    {
+        
+        //State Enum
+        /*public enum State
+        {
+            MainMenu,
+            Playing
+        }*/
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+
+        // Instatiate classes
+        Player p = new Player();
+
+        //Sets first gamestate to "Playing"
+        //State gameState = State.Playing;
+
+        // Constructor
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 480;
+        }
+
+        protected override void Initialize()
+        {
+            
+            base.Initialize();
+        }
+               
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            p.LoadContent(Content);
+        }
+
+        
+        protected override void UnloadContent()
+        {
+
+        }
+
+               protected override void Update(GameTime gameTime)
+        {
+            //Remove once we get memus working
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            p.Update(gameTime);
+
+            /*switch(gameState)
+            {
+                case State.Playing:
+                    {
+
+                    }
+            }*/
+
+            base.Update(gameTime);
+        }
+       
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            p.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ HammerGander/HammerGander/Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace HammerGander
+{
+    public class Player 
+    {
+
+        // Variables
+        public Texture2D texture;
+        public Vector2 position, facing;
+        public int speed, health;
+
+        // Rektangle
+        public Rectangle boundingBox;
+
+        // Constructor
+        public Player()
+        {
+            texture = null;
+            position = new Vector2(300, 300);
+            speed = 5;
+        }
+
+        // Load Content
+        public void LoadContent(ContentManager Content)
+        {
+            texture = Content.Load<Texture2D>("graphics/ship");
+        }
+
+        // Update
+        public void Update(GameTime gameTime)
+        {
+
+            // Getting Keyboard State / Input
+            KeyboardState keyState = Keyboard.GetState();
+
+            boundingBox = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+
+            if (keyState.IsKeyDown(Keys.W) || keyState.IsKeyDown(Keys.Up))
+            {
+                position.Y = position.Y - speed;
+            }
+
+            if (keyState.IsKeyDown(Keys.A) || keyState.IsKeyDown(Keys.Left))
+            {
+                position.X = position.X - speed;
+            }
+
+            if (keyState.IsKeyDown(Keys.S) || keyState.IsKeyDown(Keys.Down))
+            {
+                position.Y = position.Y + speed;
+            }
+
+            if (keyState.IsKeyDown(Keys.D) || keyState.IsKeyDown(Keys.Right))
+            {
+                position.X = position.X + speed;
+            }
+
+            // Keeping the player on the screen
+            if (position.X <= 0) position.X = 0;
+            if (position.X >= 800 - texture.Width) position.X = 800 - texture.Width;
+            if (position.Y <= 0) position.Y = 0;
+            if (position.Y >= 600 - texture.Height) position.Y = 600 - texture.Height;
+        }
+                
+        // Draw
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}
